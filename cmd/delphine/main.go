17d036caf5deabaddf2612543c96bdb727d31725
A-Hilaly <hilalyamine@gmail.com>
2018-07-12 21:24:01 +0200

    Init version 0.0.0

---
+++ cmd/delphine/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/medtune/delphine/pkg"
+)
+
+func main() {
+	fmt.Printf("version: %v", pkg.VERSION)
+}
---
+++ pkg/version.go
@@ -0,0 +1,5 @@
+package pkg
+
+const (
+	VERSION = "v0.0.0"
+)
